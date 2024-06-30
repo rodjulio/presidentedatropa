08f9a5782d1c756e720c2fec94f6ed5ff71cf935
rodjulio <rodrigo.henriquescampos@gmail.com>
2024-06-29 21:35:36 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function trocarFoto() {
+    var imagem = document.getElementById('foto');
+    var resposta = document.getElementById('resposta');
+    if (imagem.src.match('masaki-removebg-preview.png')) {
+        imagem.src = 'https://www.dropbox.com/scl/fi/whgy7n1e8g3hbinat96y6/rodrigo-removebg-preview.png?rlkey=lu0x2ha2dei3epawu0yjb8x07&st=9bs9npg9&dl=1';
+        resposta.innerHTML = '<strong>Presidente: Rodrigo</strong><br>Resposta Correta!';
+    } else {
+        imagem.src = 'https://www.dropbox.com/scl/fi/ntz1i6fajnpw67g0r4q7m/masaki-removebg-preview.png?rlkey=rk001hg5u68yh978kg9ps0csz&st=0mrvo9wj&dl=1';
+        resposta.innerHTML = ''; // Limpa a resposta ao voltar para a imagem inicial
+    }
+}
