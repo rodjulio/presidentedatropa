fa41c5b71a325508e18ec8693ce8827225a35781
rodjulio <rodrigo.henriquescampos@gmail.com>
2024-06-30 10:46:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,61 @@
-function trocarFoto() {
-    var imagem = document.getElementById('foto');
-    if (imagem.src.match('masaki-removebg-preview.png')) {
-        imagem.src = 'https://www.dropbox.com/scl/fi/whgy7n1e8g3hbinat96y6/rodrigo-removebg-preview.png?rlkey=lu0x2ha2dei3epawu0yjb8x07&st=9bs9npg9&dl=1'; // Link para a imagem do Rodrigo
-    } else {
-        imagem.src = 'https://www.dropbox.com/scl/fi/ntz1i6fajnpw67g0r4q7m/masaki-removebg-preview.png?rlkey=rk001hg5u68yh978kg9ps0csz&st=0mrvo9wj&dl=1'; // Volta para a imagem do Masaki
-    }
+/* Estilos para o corpo */
+body {
+    font-family: 'Roboto', sans-serif;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    margin: 0;
+    background-color: #282c34;
+    color: #61dafb;
+}
+
+/* Container central */
+.container {
+    text-align: center;
+}
+
+/* Estilos para o cabeçalho */
+.header h1 {
+    font-size: 36px;
+    color: #ffffff;
+    margin-bottom: 20px;
+}
+
+/* Estilos para a div presidente */
+.presidente {
+    cursor: pointer;
+    transition: transform 0.3s;
+    margin: 20px;
+    border: 5px solid #61dafb;
+    border-radius: 10px;
+    padding: 10px;
+    background-color: #20232a;
+}
+
+.presidente:hover {
+    transform: scale(1.1);
+}
+
+img {
+    width: 100%;
+    max-width: 300px;
+    height: auto;
+    border-radius: 10px;
+}
+
+/* Estilos para o texto do título */
+#titulo {
+    color: #ffffff;
+    font-size: 20px;
+    font-weight: bold;
+    margin-top: 10px;
+}
+
+/* Estilos para a resposta */
+#resposta {
+    color: #61dafb;
+    font-size: 18px;
+    font-weight: bold;
+    margin-top: 10px;
 }
